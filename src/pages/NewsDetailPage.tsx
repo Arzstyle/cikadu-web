b3e6a7513808b37c5e03aad2d0839b40cdf63b42
Muhamad Akbar Rizky Saputra <148222478+Arzstyle@users.noreply.github.com>
2025-08-14 14:13:21 +0700

    Updated NewsDetailPage.tsx

--- src/pages/NewsDetailPage.tsx
+++ src/pages/NewsDetailPage.tsx
@@ -469,8 +469,8 @@ const NewsDetailPage: React.FC = () => {
             </h2>
             
             <p className="text-xl md:text-2xl text-emerald-50 mb-12 max-w-3xl mx-auto leading-relaxed font-light">
-              Mari bergabung dan berpartisipasi dalam <strong className="text-white">transformasi berkelanjutan</strong> 
-                 Desa Cikadu menuju <strong className="text-white">masyarakat digital yang maju</strong> dan sejahtera.
+              Mari bergabung dan berpartisipasi dalam <strong className="text-white">transformasi berkelanjutan </strong> 
+               Desa Cikadu menuju <strong className="text-white">masyarakat digital yang maju</strong> dan sejahtera.
             </p>
           </motion.div>
 
